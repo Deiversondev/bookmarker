f3369ed25a2960fe9d634abd47bd0151d8f56431
Deiverson Cruz <dcruzbhha2@gmail.com>
2022-06-23 16:37:01 -0300

    Fiinished bookmark form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,7 +1,44 @@
+import {useFormik} from 'formik'
+
+
 const Form = () => {
+
+const formik = useFormik({
+    initialValues: {
+        name:'',
+        startDate:'',
+        numberOfPages:'',
+        finished:'false',
+        bookMark:''
+    }, onSubmit: values =>{
+        console.log(values)
+        formik.resetForm()
+    }
+})
+
+
     return(
         <div>
-            working
+                <form onSubmit={formik.handleSubmit}>
+        <div>
+                <label htmlFor="" >Name</label>
+                <input type="text" name='name' id='name' onChange={formik.handleChange} value={formik.values.name}/>
+        </div>
+        <div>
+                <label htmlFor=""></label><input type="text" id='startDate' onChange={formik.handleChange} value={formik.values.startDate} />
+        </div>
+        <div>
+                <label htmlFor=""></label><input type="text" id='numberOfPages' onChange={formik.handleChange} value={formik.values.numberOfPages} />
+        </div>
+        <div>
+                <label htmlFor=""></label><input type="text" id='finished' onChange={formik.handleChange} value={formik.values.finished} />
+        </div>
+        <div>
+                <label htmlFor=""></label><input type="text" onChange={formik.handleChange}  id='bookMark' value={formik.values.bookMark} />
+        </div>
+
+        <button type="submit">Save</button>
+                </form>
         </div>
     )
 }

b4d6080585cc940a72370c9edbcfdabb11cf1974
Deiverson Cruz <dcruzbhha2@gmail.com>
2022-06-24 12:03:20 -0300

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import Form from './pages/Form';
+import SignIn from './pages/sign-in/SignIn';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+      {/* <Form/> */}
+      <SignIn/>
     </div>
   );
 }
